fa711c4331dea66d4576a3b89bbd96b2dc8eb9f3
MARGARET MORRIS <margaretblue@MARGARETs-MacBook-Pro.local>
2015-07-08 14:33:43 -0700

    beginning game flow and StickStack methods

--- gameofsticks.py
+++ gameofsticks.py
@@ -4,21 +4,20 @@ import random
 #pit how AI
 
 class StickStack:
-    def __init__(self, how_many_sticks):
+    def __init__(self, how_many_sticks=20):
         """Creates instance of Stack of Sticks"""
         self.stick_count = how_many_sticks
 
 
-    def get_stick_count(self):
-        return self.stick_count
-
-
     def take_away_sticks(self, how_many_sticks):
         self.stick_count = self.stick_count - how_many_sticks
+        print("Debug took away {} sticks stick_count now{}".format(how_many_sticks, self.stick_count))
         return self.stick_count
+
 
     def add_sticks(self, how_many_sticks):
         self.stick_count = self.stick_count + how_many_sticks
+        print("Debug added {} sticks stick_count now{}".format(how_many_sticks, self.stick_count))
         return self.stick_count
 
 class Picks:
@@ -26,8 +25,11 @@ class Picks:
     def __init__(self):
         pass
 
+
     def set_pick(self, how_many_sticks):
+        print("Debug set_pick {}".format(how_many_sticks))
         self.stick_count = how_many_sticks
+        return self.stick_count
 
 
 
@@ -35,10 +37,25 @@ class Player:
     """Player keeps its score, its name, his sets of turns"""
     def __init__(self, name="Hal"):
         self.name = name
-        self.score = score
-        self.turns_sets = []
+        self.score = 0
+        self.picked_sets = []
 
 
+
+class Game:
+    def __init__(self, player1, player2):
+        self.player1 = player1
+        self.player2 = player2
+        current_player = player1
+
+    def start(self):
+        #creates stick_stack
+        stick_stack = StickStack()
+        number_of_sticks = stick_stack.stick_count
+        print(number_of_sticks)
+        # If stick stack is not 0, ask current player if they want to pick
+        while stick_stack != 0:
+            
 
 
 # class AIPlayer
@@ -46,9 +63,17 @@ class Player:
 #    pass
 
 
-
 if __name__ == '__main__':
-    new_stack = StickStack(20)
-    new_stack.take_away_sticks(3)
-    new_stack.add_sticks(4)
-    print(new_stack.get_stick_count())
+
+    player1 = Player("Hal")
+    player2 = Player("Marvin")
+    game = Game(player1, player2)
+    game.start()
+
+
+
+
+    # new_stack = StickStack(20)
+    # new_stack.take_away_sticks(3)
+    # new_stack.add_sticks(4)
+    # print(new_stack.get_stick_count())
