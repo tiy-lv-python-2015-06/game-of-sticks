6d2bc27d1dbd30d9028e6167f75442e279fe897c
MARGARET MORRIS <margaretblue@MARGARETs-MacBook-Pro.local>
2015-07-08 20:04:11 -0700

    human-to-human functionality complete

--- gameofsticks.py
+++ gameofsticks.py
@@ -11,8 +11,9 @@ class StickStack:
 
     def take_away_sticks(self, how_many_sticks):
         self.stick_count = self.stick_count - how_many_sticks
-        print("Debug took away {} sticks stick_count now{}".format(how_many_sticks, self.stick_count))
+        print("Took away {}, there are {} sticks in the stack now. \n".format(how_many_sticks, self.stick_count))
         return self.stick_count
+
 
     # Don't make a get_stick_count() method, instead call stick_stack.stick_count attribute
 
@@ -21,46 +22,6 @@ class StickStack:
         self.stick_count = self.stick_count + how_many_sticks
         print("Debug added {} sticks stick_count now{}".format(how_many_sticks, self.stick_count))
         return self.stick_count
-
-class Turn:
-
-    def __init__(self, player, stick_stack):
-        self.player = player
-        self.stick_stack = stick_stack
-        self.turn_over = False
-
-
-    def set_choice(self, number_chosen):
-        # have that choice go to set_choice which updates player score and updates stick_stack.count and turn count
-        self.player.score += number_chosen
-        #update stick stack
-        self.stick_stack.take_away_sticks(number_chosen)
-
-
-    def run(self):
-        while not self.turn_over:
-            self.go()   #this isolates the loop into one method for testing
-
-
-    def go(self):
-        #if player eligible
-        if self.player.score < 20:
-            # tell player how many sticks she has
-            print("You have {} sticks.\n".format(self.player.score))
-            print("-- " * self.player.score)
-            # tell player how many sticks in overall stick_stack
-            print("There are {} sticks in the Stick Stack \n".format(self.stick_stack.stick_count))
-            print("-- " * self.stick_stack.stick_count)
-            # ask player to choose number of sticks
-            number_chosen = self.player.choose_number_of_sticks()
-            # have that choice go to set_choice which updates player score and updates stick_stack.count and turn count
-            self.set_choice(number_chosen)
-            #turn is over, switch players
-            self.turn_over = True
-                #switch players
-                # How to switch players
-
-
 
 
 class Player:
@@ -71,40 +32,75 @@ class Player:
         self.score = 0
         self.picked_sets = []
 
+
     def choose_number_of_sticks(self):
-        raw_choice = input("How many sticks do you want? (If you want to pass, pick 0)\n")
+        print("Your turn, {}.\n".format(self.name))
+        raw_choice = input("How many sticks do you want? > \n")
         number_chosen = int(raw_choice)
         return number_chosen
+
 
 class Game:
     def __init__(self, player1, player2):
         self.player1 = player1
         self.player2 = player2
         self.current_player = player1
+        self.player = self.current_player
+        self.stick_stack = StickStack()
+        self.winner = None
 
 
     def change_current_player(self):
         if self.current_player == self.player1:
             self.current_player = self.player2
+            self.player = self.current_player
         else:
             self.current_player = self.player1
+            self.player = self.current_player
+
+
+    def set_choice(self, number_chosen):
+        # have that choice go to set_choice which updates stick_stack.count and turn count
+        # update stick_stack
+        self.stick_stack.take_away_sticks(number_chosen)
 
 
     def start(self):
         #creates stick_stack
-        stick_stack = StickStack()
-        number_of_sticks = stick_stack.stick_count
-        print(number_of_sticks)
+        print("stick_stack stick_count is {}".format(self.stick_stack.stick_count))
         # If stick stack is not 0, ask current player if they want to pick
-        while stick_stack != 0:
-            #instantiate a turn
-            turn = Turn(self.current_player, stick_stack)
-            #turn.run() is going to call turn.go() which will do most of the work
-            while not turn.turn_over:
-                turn.run()
-                #self.current_player.choose_pass_or_play()
+        if self.stick_stack.stick_count != 0:
+            #game.run() is going to call game.go() which will do most of the work
+            self.run()
+
+    def you_lose(self):
+        print("Sorry, {}, you lose.".format(self.current_player.name))
+        if self.current_player == self.player1:
+            self.winner = self.player2.name
+        elif self.current_player == self.player2:
+            self.winner = self.player1.name
+        print("{} Wins!".format(self.winner))
 
 
+    def run(self):
+        self.go()   #this isolates the loop into one method for testing
+
+
+    def go(self):
+        #if player eligible
+        while self.stick_stack.stick_count <= 20 and self.stick_stack.stick_count > 1:
+            print("-- " * self.stick_stack.stick_count)
+            # tell player how many sticks in overall stick_stack
+            print("There are {} sticks in the Stick Stack \n".format(self.stick_stack.stick_count))
+            # ask player to choose number of sticks
+            number_chosen = self.player.choose_number_of_sticks()
+            # have that choice go to set_choice which and updates stick_stack.count and turn count
+            self.set_choice(number_chosen)
+            #change players
+            self.change_current_player()
+        if self.stick_stack.stick_count < 1:
+            self.you_lose()
+            print("You lose, {}".format(self.current_player))
 
 
 
